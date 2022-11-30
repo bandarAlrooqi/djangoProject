2899d0f2f95ec7dbe1ce8a580721236c7e1b7d24
Bandar Alrooqi <officialAlrooqi@gmail.com>
2022-11-30 11:30:44 +0300

    update admin page

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'unit_price', 'inventory_status', 'collection']
+    list_editable = ['unit_price']
+    list_per_page = 10
+    @admin.display(ordering='inventory')
+    def inventory_status(self, obj):
+        if obj.inventory > 0:
+            return 'In Stock'
+        return 'Out of Stock'
+    # for more info visit https://docs.djangoproject.com/en/4.0/ref/contrib/admin/
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name']
+    list_per_page = 10
+    # for more info visit https://docs.djangoproject.com/en/4.0/ref/contrib/admin/
+
+
+admin.site.site_header = 'Store Admin'
+admin.site.index_title = 'Admin'
+admin.site.register(models.Collection)
+admin.site.register(models.Order)
+admin.site.register(models.OrderItem)
--- store/models.py
+++ store/models.py
@@ -11,6 +11,13 @@ class Collection(models.Model):
     featured_product = models.ForeignKey(
         'Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = 'Collections'
+        ordering = ['title']
+
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -21,6 +28,13 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
+        verbose_name_plural = 'Products'
 
 
 class Customer(models.Model):
@@ -41,6 +55,13 @@ class Customer(models.Model):
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+        verbose_name_plural = 'Customers'
+
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
@@ -57,12 +78,26 @@ class Order(models.Model):
         max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
+    def __str__(self):
+        return f'Order {self.id} placed at {self.placed_at} by {self.customer.first_name} {self.customer.last_name}'
+
+    class Meta:
+        ordering = ['-placed_at']
+        verbose_name_plural = 'Orders'
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
     product = models.ForeignKey(Product, on_delete=models.PROTECT)
     quantity = models.PositiveSmallIntegerField()
     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.quantity} of {self.product.title}'
+
+    class Meta:
+        verbose_name_plural = 'Order Items'
+        ordering = ['product__title']
 
 
 class Address(models.Model):
